da652fd3887c6caed20418a5d453467d62dd7668
Justin Lee <justinrhee1114@gmail.com>
2021-04-26 18:35:58 -0700

    'fixed'

--- evaluating.py
+++ evaluating.py
@@ -47,7 +47,7 @@ def results():
                 if label == prediction:
                     correct_pred[classes[label]] += 1
                 total_pred[classes[label]] += 1
-                
+
     print('-' * 8, 'Accuracy per class', '-'  * 8) 
     for classname, correct_count in correct_pred.items():
         accuracy = 100 * float(correct_count) / total_pred[classname]
@@ -90,5 +90,35 @@ if __name__ == '__main__':
 
 
 '''
-
+-------- Accuracy per class --------
+Accuracy for letter A     is: 100.0 %
+Accuracy for letter B     is: 99.1 %
+Accuracy for letter C     is: 98.4 %
+Accuracy for letter D     is: 99.2 %
+Accuracy for letter E     is: 99.8 %
+Accuracy for letter F     is: 99.6 %
+Accuracy for letter G     is: 94.3 %
+Accuracy for letter H     is: 95.4 %
+Accuracy for letter I     is: 98.3 %
+Accuracy for letter K     is: 98.5 %
+Accuracy for letter L     is: 99.5 %
+Accuracy for letter M     is: 94.9 %
+Accuracy for letter N     is: 94.5 %
+Accuracy for letter O     is: 98.8 %
+Accuracy for letter P     is: 100.0 %
+Accuracy for letter Q     is: 98.8 %
+Accuracy for letter R     is: 97.2 %
+Accuracy for letter S     is: 92.7 %
+Accuracy for letter T     is: 77.0 %
+Accuracy for letter U     is: 95.5 %
+Accuracy for letter V     is: 97.1 %
+Accuracy for letter W     is: 100.0 %
+Accuracy for letter X     is: 98.9 %
+Accuracy for letter Y     is: 95.8 %
+---------- PyTorch Model ----------
+Training accuracy: 99.8
+Validation accuracy: 96.9
+----------  ONNX Model  ----------
+Training accuracy: 99.8
+Validation accuracy: 96.9
 '''
